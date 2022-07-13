3599ca4137254fe3427f3d51a43a9c9e0eef96f8
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2022-07-13 18:47:53 +0530

    loo eval over sampled negatives

--- src/evaluate.py
+++ src/evaluate.py
@@ -22,3 +22,49 @@ def ndcg(ranked: List[int], target: int) -> float:
         return 0.0
     rank = ranked.index(target)
     return 1.0 / math.log2(rank + 2)
+
+
+def evaluate_loo(
+    model: torch.nn.Module,
+    test_df,
+    user_pos: Dict[int, set],
+    num_items: int,
+    top_k: int = 10,
+    num_negatives: int = 99,
+    device: str = "cpu",
+    seed: int = 7,
+) -> Tuple[float, float]:
+    """Run leave-one-out evaluation.
+
+    For each (user, positive_item) row in `test_df`, sample
+    `num_negatives` items the user has not seen, score the candidate
+    set with the model, and compute HR@K and NDCG@K.
+    """
+    model.eval()
+    rng = np.random.default_rng(seed)
+    hits: List[int] = []
+    ndcgs: List[float] = []
+
+    with torch.no_grad():
+        for _, row in test_df.iterrows():
+            u = int(row["user_idx"])
+            pos = int(row["item_idx"])
+            seen = user_pos.get(u, set())
+
+            negatives: List[int] = []
+            while len(negatives) < num_negatives:
+                j = int(rng.integers(0, num_items))
+                if j != pos and j not in seen and j not in negatives:
+                    negatives.append(j)
+            candidates = [pos] + negatives
+
+            users_t = torch.full((len(candidates),), u, dtype=torch.long, device=device)
+            items_t = torch.tensor(candidates, dtype=torch.long, device=device)
+            scores = model(users_t, items_t).detach().cpu().numpy()
+
+            order = np.argsort(-scores)
+            ranked = [candidates[k] for k in order[:top_k]]
+            hits.append(hit_rate(ranked, pos))
+            ndcgs.append(ndcg(ranked, pos))
+
+    return float(np.mean(hits)), float(np.mean(ndcgs))
